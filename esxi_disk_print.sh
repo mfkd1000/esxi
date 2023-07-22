315ba4ef61ba647a42bfa1316c156f2be9ca3e2f
mfkd1000 <73184372+mfkd1000@users.noreply.github.com>
2023-07-22 23:07:42 +0800

    Add files via upload

---
+++ esxi_disk_print.sh
@@ -0,0 +1,24 @@
+﻿#!/bin/bash
+
+# Script to obtain the placement of the physical disk by naa on ESXi hosts
+# Author: Jorluis Perales, VxRail TSE 2 @ Dell EMC
+# Version 1.0
+#
+# Do not change anything below this line
+# --------------------------------------
+
+echo "=============Physical disks placement=============="
+echo ""
+	
+esxcli storage core device list | grep "naa" | awk '{print $1}' | grep "naa" | while read in; do
+
+echo "$in"
+esxcli storage core device physical get -d "$in"
+sleep 1
+
+echo "===================================================="
+
+done
+
+
+rm -rf esxi_disk_print.sh
